81ad85ee6ec716f1dfe54f3a963e1cc0a9c41af1
Rickyy <axrickyy@proton.me>
2025-05-12 18:40:01 +0530

    Refined

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const resultTime = document.querySelector(`.time`);
 
 // * Game Variables
 let running = false;
-let questionIndex = 10;
+let questionIndex = 0;
 let score = 0;
 let answered = 0;
 let timeElapsed = 0;
